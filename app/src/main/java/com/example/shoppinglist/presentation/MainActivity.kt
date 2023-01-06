50064a0f81df48c2512a97d04e2acb683b228521
dangaliu <ii4603399@gmail.com>
2023-01-06 19:22:07 +0300

    MainViewModel created

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        viewModel.getShopList()
+
+        viewModel.shopList.observe(this) {
+            Log.d(TAG, it.toString())
+        }
     }
 }
---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.*
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun editShopItem(shopItem: ShopItem) {
+        editShopItemUseCase.editShopItem(shopItem)
+        getShopList()
+    }
+}
