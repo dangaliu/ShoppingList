60020f6afb1dcdb9a366e0d51c60ca521ad5d6e3
dangaliu <ii4603399@gmail.com>
2023-01-07 16:46:57 +0300

    list implementation through linear layout

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -8,12 +8,14 @@ import com.example.shoppinglist.domain.ShopListRepository
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 ->
+        o1.id.compareTo(o2.id)
+    })
 
     private var autoIncrementId = 0
 
     init {
-        for (i in 0..10) {
+        for (i in 0..25) {
             addShopItem(ShopItem("name $i", i, true))
         }
     }
--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.shoppinglist.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.ActivityMainBinding
+import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,15 +15,39 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         viewModel.shopList.observe(this) {
+            showList(it)
             Log.d(TAG, it.toString())
         }
+    }
 
+    private fun showList(shopList: List<ShopItem>) {
+        binding.llShoppingList.removeAllViews()
+        for (shopItem in shopList) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = layoutInflater.inflate(layoutId, binding.llShoppingList, false)
+            view.setOnLongClickListener {
+                viewModel.changeEnabledState(shopItem)
+                true
+            }
+            val tvName = view.findViewById<TextView>(R.id.tvTitle)
+            val tvCount = view.findViewById<TextView>(R.id.tvCount)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            binding.llShoppingList.addView(view)
+        }
     }
 }
